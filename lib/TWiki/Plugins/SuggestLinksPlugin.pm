23bbefab067b6d92780b497b3b3fab67b119d58a
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/SuggestLinksPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/SuggestLinksPlugin.pm
+++ lib/TWiki/Plugins/SuggestLinksPlugin.pm
@@ -9,19 +9,19 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
-# http://www.gnu.ai.mit.edu/copyleft/gpl.html 
+# GNU General Public License for more details, published at
+# http://www.gnu.ai.mit.edu/copyleft/gpl.html
 #
 # =========================
 #
 # This plugin finds all group of words that could be joined to
 # link to an existing topic.
 #
-# E.g. if a topic named ANiceTopic exists then 
+# E.g. if a topic named ANiceTopic exists then
 #	"topic is nice" is hilighted
 #
 # The rationale is that you often dont' know that there is some
-# topic speaking of something you are writing about, and that 
+# topic speaking of something you are writing about, and that
 # probably you would like to link it.
 #
 # This is probably useful only during Preview.
@@ -39,11 +39,12 @@
 #
 # =========================
 package TWiki::Plugins::SuggestLinksPlugin;
+
 # =========================
 use vars qw( $web $topic $user $installWeb
-	    %allTopicNamePatterns 
-	    $stopWords $format1 $format2
-	    $maxWords $selectedWeb );
+  %allTopicNamePatterns
+  $stopWords $format1 $format2
+  $maxWords $selectedWeb );
 
 # we filter single characters, articles ...
 # $stopWords = qr /[a-zA-Z0-9]|an|the|and|or|not|to|for/i ;
@@ -53,116 +54,125 @@ $maxWords = 4;
 
 # use List::Permutor; # lazy load
 # =========================
-sub initPlugin
-{
+sub initPlugin {
     ( $topic, $web, $user, $installWeb ) = @_;
 
     my $query = &TWiki::Func::getCgiQuery();
-    if( ! $query ) { return 0; }
-                
+    if ( !$query ) { return 0; }
+
     # see if they want me
-    my $yn    = $query->param('suggestions') || 'off';
+    my $yn = $query->param('suggestions') || 'off';
     if ( $yn ne 'on' ) { return 0; }
 
     # see if List::Permutor is present
     eval 'require List::Permutor';
-    if (!defined $List::Permutor::VERSION) {
-        &TWiki::Func::writeWarning('SuggestLinksPlugin init failed: missing List::Permutor');
+    if ( !defined $List::Permutor::VERSION ) {
+        &TWiki::Func::writeWarning(
+            'SuggestLinksPlugin init failed: missing List::Permutor');
         return 0;
     }
 
     # from where should we get the topic names?
-    $selectedWeb = $query->param( 'suggestionsWeb' ) || $web;
+    $selectedWeb = $query->param('suggestionsWeb') || $web;
 
     # how long can be the patterns?
-    $maxWords    = scalar ($query->param( 'suggestionsLength' ) || '4');
-    if ($maxWords > 6) {
-	$maxWords = 6;
+    $maxWords = scalar( $query->param('suggestionsLength') || '4' );
+    if ( $maxWords > 6 ) {
+        $maxWords = 6;
     }
 
-    $stopWords = &TWiki::Func::getPreferencesValue('SUGGESTLINKSPLUGIN_STOPWORDS')
-			|| '[:lower:]' ;
-    $stopWords =~ s/[,\s]+/|/go ;
-    $stopwords =~ s/\|+/\|/go ;
-    $stopWords = qr /$stopWords/i ; 
+    $stopWords =
+      &TWiki::Func::getPreferencesValue('SUGGESTLINKSPLUGIN_STOPWORDS')
+      || '[:lower:]';
+    $stopWords =~ s/[,\s]+/|/go;
+    $stopwords =~ s/\|+/\|/go;
+    $stopWords = qr /$stopWords/i;
 
     $format1 = &TWiki::Func::getPreferencesValue('SUGGESTLINKSPLUGIN_FORMAT1')
-			|| "<SPAN STYLE=\"background : yellow;\">";
+      || "<SPAN STYLE=\"background : yellow;\">";
     $format2 = &TWiki::Func::getPreferencesValue('SUGGESTLINKSPLUGIN_FORMAT2')
-			|| "<\/SPAN><img src=\"%PUBURLPATH%\/$installWeb\/SuggestLinksPlugin\/exclam.gif\" alt=\"\$web.\$topic\">" ;
+      || "<\/SPAN><img src=\"%PUBURLPATH%\/$installWeb\/SuggestLinksPlugin\/exclam.gif\" alt=\"\$web.\$topic\">";
 
-    foreach $name (&TWiki::Func::getTopicList($selectedWeb, $user)) {
-	my $pats = &getAllPatterns($name);
-	$allTopicNamePatterns{$name} = $pats;
-    };
+    foreach $name ( &TWiki::Func::getTopicList( $selectedWeb, $user ) ) {
+        my $pats = &getAllPatterns($name);
+        $allTopicNamePatterns{$name} = $pats;
+    }
 
     return 1;
 }
+
 # =========================
 #sub outsidePREPreviewHandler
-sub commonTagsHandler
-{
-#    my ( $text, $topic, $web ) = @_;
-    while (($name, $patterns) = each %allTopicNamePatterns) {
-	foreach $pattern ( @{$patterns} ) {
-	    my ($f1, $f2) = ("$format1", "$format2");
-	    $f1 =~ s/\$topic/$name/g ;
-	    $f2 =~ s/\$topic/$name/g ;
-	    $f1 =~ s/\$web/$selectedWeb/go ;
-	    $f2 =~ s/\$web/$selectedWeb/go ;
-	    $_[0] =~ s/$pattern/$1$f1$2$f2/gi ;
-	}
+sub commonTagsHandler {
+
+    #    my ( $text, $topic, $web ) = @_;
+    while ( ( $name, $patterns ) = each %allTopicNamePatterns ) {
+        foreach $pattern ( @{$patterns} ) {
+            my ( $f1, $f2 ) = ( "$format1", "$format2" );
+            $f1   =~ s/\$topic/$name/g;
+            $f2   =~ s/\$topic/$name/g;
+            $f1   =~ s/\$web/$selectedWeb/go;
+            $f2   =~ s/\$web/$selectedWeb/go;
+            $_[0] =~ s/$pattern/$1$f1$2$f2/gi;
+        }
     }
 }
+
 # =========================
-sub getAllPatterns
-{
-    my ($name, $web) = @_;
-    
+sub getAllPatterns {
+    my ( $name, $web ) = @_;
+
     my $words = $name;
-    $words =~ s/([A-Z][a-z])/ $1/go;    
-    my  @words = split( " ", $words);
+    $words =~ s/([A-Z][a-z])/ $1/go;
+    my @words = split( " ", $words );
 
     # we filter out stopwords (articles ...)
-    @words = grep {! /^$stopWords$/i } @words;
+    @words = grep { !/^$stopWords$/i } @words;
 
     # we never admit more than 6 words (6! = 720 permutations)
-    my $theMax = ($maxWords < 7) ? $maxWords : 6;
+    my $theMax = ( $maxWords < 7 ) ? $maxWords : 6;
 
     # we limit list size
-    if (scalar( @words ) > $theMax ) {
-	#FIXME: should we truncate instead than returning null?
-	return ();
+    if ( scalar(@words) > $theMax ) {
+
+        #FIXME: should we truncate instead than returning null?
+        return ();
     }
 
     #FIXME: are the plurals correct?
     map {
-    # plural --> plural|singular
-    $_ =~ s/^(([^|]+)ies)$/$1|$2y/o;
-    $_ =~ s/^(([^|]+)sses)$/$1|$2ss/o;
-    $_ =~ s/^(([^|]+)xes)$/$1|$2x/o;
-    $_ =~ s/^(([^|]*[^|s])s)$/$1|$2/o;
 
-    # singular --> singular|plural
-    $_ =~ s/^(([^|]+)y)$/$1|$2ies/o;
-    $_ =~ s/^(([^|]+)ss)$/$1|$2sses/o;
-    $_ =~ s/^(([^|]+)x)$/$1|$2xes/o;
-    $_ =~ s/^([^|]*[^|s])$/$1|$1s/o;
-    } @words ;
+        # plural --> plural|singular
+        $_ =~ s/^(([^|]+)ies)$/$1|$2y/o;
+        $_ =~ s/^(([^|]+)sses)$/$1|$2ss/o;
+        $_ =~ s/^(([^|]+)xes)$/$1|$2x/o;
+        $_ =~ s/^(([^|]*[^|s])s)$/$1|$2/o;
 
-    return &getAllPermPatterns(@words);    
+        # singular --> singular|plural
+        $_ =~ s/^(([^|]+)y)$/$1|$2ies/o;
+        $_ =~ s/^(([^|]+)ss)$/$1|$2sses/o;
+        $_ =~ s/^(([^|]+)x)$/$1|$2xes/o;
+        $_ =~ s/^([^|]*[^|s])$/$1|$1s/o;
+    } @words;
+
+    return &getAllPermPatterns(@words);
 }
+
 # =========================
-sub getAllPermPatterns
-{
+sub getAllPermPatterns {
     my $perm = new List::Permutor @_;
     my @pats = ();
     my $pat  = "";
-    while (my @set = $perm->next) {
-	#we look  for permutations interspersed with at most 2 words here and there
-	$pat = '([\s\.\,\:\!\?^])((' . join( ')\s+([a-zA-Z0-9]+\s+)?([a-zA-Z0-9]+\s+)?(', @set) . ')[\s\.\,\:\!\?$])';
-	# we want them caseless
-	push @pats, qr /$pat/i ;
+    while ( my @set = $perm->next ) {
+
+     #we look  for permutations interspersed with at most 2 words here and there
+        $pat =
+            '([\s\.\,\:\!\?^])(('
+          . join( ')\s+([a-zA-Z0-9]+\s+)?([a-zA-Z0-9]+\s+)?(', @set )
+          . ')[\s\.\,\:\!\?$])';
+
+        # we want them caseless
+        push @pats, qr /$pat/i;
     }
     return \@pats;
 }
